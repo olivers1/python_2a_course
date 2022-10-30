3c69a6f3ec37ec0405a4b26375992c42e185de29
olivers1 <oliver.staberg@gmail.com>
2022-10-30 14:01:14 +0100

    excercise 2a done

--- excercise_2a.py
+++ excercise_2a.py
@@ -1,12 +1,12 @@
+from re import A
+from unicodedata import name
+
+
 class Contact:
     def __init__(self, name, number, age):
         self._name = name
         self._number = number
         self._age = age
-    
-    @property
-    def get_name(self):
-        return self._name
     
     @property
     def get_age(self):
@@ -22,17 +22,19 @@ class PhoneBook(Contact):
         self._contacts = []
 
     def add_contact(self, contact: Contact):
-        if contact in self._contacts:
-            self._contacts = contact
+        for i, item in enumerate(self._contacts):
+            if item._name == contact._name:
+                self._contacts[i] = contact
+                break
         else:
             self._contacts.append(contact)
     
     def get_contact(self, name):
         for item in self._contacts:
-            if item.get_name == name:
-                return print("name:", item.get_name, ", number:", item.get_number, ", age:", item.get_age)
-            else:
-                print("name is not in the list")
+            if item._name == name:
+                return print("name:", item._name, ", number:", item.get_number, ", age:", item.get_age)
+        else:
+            print("name is not in the list")
     
     def __len__(self):
         if len(self._contacts) > 0:
@@ -47,19 +49,14 @@ age_list = [36, 32, 28, 12]
 
 pbook = PhoneBook()
 
-for i in range(0, 3):
+for i in range(0, 4):
     pbook.add_contact(Contact(name_list[i], numb_list[i], age_list[i]))
 
 
-# code not working, not replacing object with new object if having same name. print out all object when created to see if problem with list
+for i in pbook._contacts:
+    if i._name == "ola":
+        print(i._name)
 
-"""
-for i in range(0, pbook.__len__):
-    print(pbook[i].get_name)
-"""
 
 print(pbook.__len__())
-pbook.get_contact("oliver")
-
-
-
+pbook.get_contact("ola")
