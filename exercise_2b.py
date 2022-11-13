a97ca97768ca67544965b9d1aab1cc1038f2d94e
olivers1 <oliver.staberg@gmail.com>
2022-11-13 13:34:51 +0100

    excercise 2b done

--- exercise_2b.py
+++ exercise_2b.py
@@ -1,6 +1,7 @@
+
 class FightActions:
     def fight(self, defender):
-        defender.power_level -= self.hit_points
+        pass
 
 
 class CharacterAttributes:
@@ -9,26 +10,65 @@ class CharacterAttributes:
         self.hit_points = hit_points
 
 
-class Batman(CharacterAttributes, FightActions):
+class Batman(FightActions, CharacterAttributes):
     def __init__(self):
-        self.set_stats(60, 10)
+        self.set_stats(60, 12)
 
-    def ability(self):
-        self.hit_points += 5
-
-
-class SpiderMan(CharacterAttributes, FightActions):
-    def __init__(self):
-        self.set_stats(80, 22)
-
-    def ability(self):
+    def call_alfred(self):
         self.power_level += 10
+        print("protection gadget delivered")
 
-character_attributes = CharacterAttributes()
-spider_man, batman = SpiderMan(), Batman()
 
-batman.ability()
-spider_man.ability()
-batman.fight(spider_man)
-print("batman health:", batman.hit_points)
-print("spider_man health:", spider_man.power_level)
+class SpiderMan(FightActions, CharacterAttributes):
+    def __init__(self):
+        self.set_stats(80, 9)
+
+    def go_web_go(self):
+        self.hit_points += 9
+        print("spidey hit points increased")
+
+
+batman = Batman()
+spiderman = SpiderMan()
+
+batman.call_alfred()
+spiderman.go_web_go()
+
+print("batman power lever:", batman.power_level)
+print("spiderman hitpoints:", spiderman.hit_points)
+
+
+
+# duck typing
+"""
+class Duck:
+    def walk_and_talk(self):
+        print("duck walk and quack")
+
+
+class Bird:
+    def walk_and_talk(self):
+        print("bird fly and twitter")
+
+
+class Frog:
+    def jump_and_talk(self):
+        print("frog jump and quack")
+
+
+def animal_action(animal):
+    animal.walk_and_talk()
+
+
+duck = Duck()
+bird = Bird()
+frog = Frog()
+
+animal_action(duck)
+animal_action(bird)
+
+try:
+    animal_acrion(frog)
+except AttributeError as error:
+    print(error)
+"""
